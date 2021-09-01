d0e5ef862ca7041e1751ccc1847d8e4b1f16907c
Gentrit Abazi <gentritabazi01@gmail.com>
2021-09-01 21:28:24 +0200

    Create axios client to handle errors from backend.

---
+++ src/utils/axios-client.js
@@ -0,0 +1,37 @@
+// Import
+import axios from "axios";
+
+// Request Interceptor
+axios.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      // Backend error
+      if (500 === error.response.status) {
+        alert("There is something wrong on backend!");
+      }
+
+      // 404
+      if (error.response.status == 404) {
+        alert("Page not found!");
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+// Export axios
+export default axios;
