acd1962747673b4395a7f172c486358a96710cbb
Gentrit Abazi <gentritabazi01@gmail.com>
2021-09-02 16:57:24 +0200

    Create RecipeDuration.spec.js

---
+++ tests/unit/components/RecipeDuration.spec.js
@@ -0,0 +1,14 @@
+import { shallowMount } from "@vue/test-utils";
+import RecipeDuration from "@/components/PremiumRecipeCard/components/RecipeDuration";
+
+describe("RecipeDuration.vue", () => {
+  it("Calculates 65 minutes correctly", () => {
+    const wrapper = shallowMount(RecipeDuration, {
+      propsData: {
+        minutes: 65,
+      },
+    });
+
+    expect(wrapper.find(".recipe-duration").text()).toEqual("1h 5m");
+  });
+});
